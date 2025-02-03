023abc9fc6902f38b863faf181d0e32c68c40152
Asafe <133934794+asafehackerman@users.noreply.github.com>
2025-02-02 21:22:18 -0300

    Delete projeto-integrador-asafe-elias-marques - backup directory

--- projeto-integrador-asafe-elias-marques - backup/demo/src/main/java/com/example/models/entities/PersonagemVantagem.java
+++
@@ -1,9 +0,0 @@
-package com.example.models.entities;
-
-public class PersonagemVantagem extends PersonagemRelacionamento {
-    
-    public PersonagemVantagem(int id_personagem_fk_pk, int id_vantagem_fk_pk) {
-        super(id_personagem_fk_pk, id_vantagem_fk_pk);
-    }
-    
-}
