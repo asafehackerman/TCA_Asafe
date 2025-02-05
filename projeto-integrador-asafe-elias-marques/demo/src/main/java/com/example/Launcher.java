7d09af299e412ea62cbc21142ec3dab70029cab7
Asafe <133934794+asafehackerman@users.noreply.github.com>
2025-02-04 22:13:46 -0300

    Delete projeto-integrador-asafe-elias-marques directory

--- projeto-integrador-asafe-elias-marques/demo/src/main/java/com/example/Launcher.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class Launcher {
-    public static void main(String[] args) {
-        App.launch(App.class, args);
-    }
-}
--- projeto-integrador-asafe-elias-marques/demo/src/main/java/com/example/models/entities/Desvantagem.java
+++
@@ -1,9 +0,0 @@
-package com.example.models.entities;
-
-public class Desvantagem extends Vantagem {
-
-    public Desvantagem(int id, String nome, String desc, int custo) {
-        super(id, nome, desc, custo);
-    }
-    
-}
